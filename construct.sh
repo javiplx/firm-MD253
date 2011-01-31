4afefa2aa647b7d42f4d130df3d39f99aac87e93
Javier Palacios <javiplx@gmail.com>
2011-01-31 17:49:35 +0100

    Move some extra files into boot image

--- construct.sh
+++ construct.sh
@@ -11,9 +11,30 @@ ln -s bin bootfs/sbin
 
 unsquashfs -d rootfs filesystem.bin
 
+mv bootfs/var/www/ rootfs
+ln -s ../usr/www bootfs/var/www
+
+mv rootfs/bin/hotplug bootfs/bin
+
+mv rootfs/lib/modules bootfs/usr/lib
+
 mv rootfs/lib/*-0.9.29.so bootfs/usr/lib
 mv rootfs/lib/ld-uClibc.so.0 rootfs/lib/libc.so.0 rootfs/lib/libcrypt.so.0 rootfs/lib/libm.so.0 \
         rootfs/lib/libdl.so.0 rootfs/lib/libnsl.so.0 rootfs/lib/libresolv.so.0 \
         rootfs/lib/libpthread.so.0 rootfs/lib/librt.so.0 rootfs/lib/libutil.so.0 \
         bootfs/usr/lib
+
+mkdir bootfs/usr/bin
+ln -s bin bootfs/usr/sbin
+
+mv rootfs/bin/busybox bootfs/usr/bin
+ls -al rootfs/bin | awk 'NF==11 && $11=="busybox" { printf "mv -f rootfs/bin/%s bootfs/usr/bin\n" , $9 }' | sh
+
+mv rootfs/bin/syslogd rootfs/bin/klogd bootfs/usr/bin
+mv rootfs/bin/buttons_daemon rootfs/bin/udevd bootfs/usr/bin
+mv rootfs/bin/mdadm rootfs/bin/iconv bootfs/usr/bin
+mv rootfs/bin/flashcp rootfs/bin/replaceFile bootfs/usr/bin/
+
+# At least hdparm and smartclt should be on boot image. Maybe also date, find, ntpdate, logrotate & fsck.hfsplus
+# But there is no single kb free on the image
 
